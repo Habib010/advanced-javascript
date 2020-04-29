ea5a07a368405492da072b8c354cd68216543d71
Habib010 <ahsanhabiib.du@yahoo.com>
2020-04-29 13:34:37 +0600

    null vs undefined is done

---
+++ null_vs_undefined.js
@@ -0,0 +1,23 @@
+//let pakhi;
+
+
+
+// function add (num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+
+// function add(num1, num2=22){
+//     console.log(num1 , num2);
+// }
+// const man = {name: "Abul", phone:-19899982};
+
+// console.log(man.gf);
+
+let fun = undefined;
+
+console.log(fun);
+
+let ages = [22, 44, 55];
+console.log(ages[11]);
